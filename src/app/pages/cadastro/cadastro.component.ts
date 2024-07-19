2344079352a64b0af16a21f009b0e4c9d73d8347
alicia-bea <silvaaliciabeatriz066@gmail.com>
2024-07-19 16:12:15 -0300

    Alterações no quiz, e nas telas do forum

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -73,6 +73,7 @@ cadastrar(){
     error: (erro) => {
       this.message.add({severity:'error', summary: 'Erro', detail: 'Não foi possivel fazer o cadastro' })
       console.log(erro);
+      console.log(this.request)
     }
   })
 }
