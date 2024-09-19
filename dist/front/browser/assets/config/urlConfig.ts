39c23b6cb33b75c263d1ffb228e19a8c97099bd9
Raquel Tsuji <raqueltsuji@gmail.com>
2024-09-19 14:10:33 -0300

    adicionando pasta dist

---
+++ dist/front/browser/assets/config/urlConfig.ts
@@ -0,0 +1,17 @@
+export const urlConfig = {
+  cadastrarUsuario : 'usuarios/cadastrar',
+  verificarEmail: 'email/verificacao',
+  logar: 'usuarios/logar',
+  alterarPerfil: 'usuarios/mudar',
+  alterarSenha: 'usuarios/mudar/senha',
+  deletar: 'usuarios/deletar',
+  adicionarPergunta: 'perguntas/adicionar',
+  perguntasCurso: 'perguntas/curso',
+  perguntasUsuario:'perguntas/usuario',
+  pergunta: 'perguntas',
+  adicionarResposta: 'respostas/adicionar',
+  getRespostasUsuario: 'respostas/usuario',
+  getRespostas: 'respostas/perguntas'
+
+
+}
