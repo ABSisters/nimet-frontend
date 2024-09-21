5dcd002810e42a63757dd7da1ae986bd1941bc9d
Raquel Tsuji <raqueltsuji@gmail.com>
2024-09-21 20:07:53 -0300

    teste 3

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment ={
   baseUrl: '',
-  baseApiUrl: 'http://nimet-backend-arg0hndeabcpdch9.brazilsouth-01.azurewebsites.net/api'
+  baseApiUrl: 'https://nimet-backend-arg0hndeabcpdch9.brazilsouth-01.azurewebsites.net/api'
 }
